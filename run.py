a8209099438d2f332dd2df021aeb76b05059c493
Joannamcgallagher <>
2023-04-24 19:33:31 +0000

    Add validation to the user inputted data

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-#import the os so the screen can be cleard
+# import the os so the screen can be cleard
 import os
 
 SCOPE = [
@@ -14,14 +14,10 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("fuelCardAnalysis")
 
-# data = SHEET.worksheet("Data")
-
-# d1 = data.get_all_values()
-# print(d1)
 
 def clearConsole():
     """
-    Function to clear the terminal for the user
+    Function to clear the terminal for the user 
     https://www.delftstack.com/howto/python/python-clear-console/?utm_content=cmp-true
     """
     command = "clear"
@@ -32,7 +28,8 @@ def clearConsole():
 
 def getUserFunction():
     """
-    Determine what the user would like to do - enter in data or view the analysis from the survey
+    Determine what the user would like to do - enter in data or view the 
+    analysis from the survey
     """
     print("Welcome to the Fuel Card analysis program!\nHere you can enter in data from a survery received or view the analysis from the data already received.\n\n")
     while True:
@@ -44,7 +41,7 @@ def getUserFunction():
             break
     return userChoice
 
-    
+  
 def validateUserChoice(choice):
     """
     Take in the users choice as a parameter and using validation, ensure that the choice is correct
@@ -63,9 +60,9 @@ def getUserDataInput():
     Function to retrieve the data the user inputs when they have selected to 
     input more survery data
     """   
-    #Use a while true loop so the program can check that the data entered is valid
+    # Use a while true loop so the program can check that the data entered is valid
     while True: 
-        customerDetails = [] #array to store the customer details
+        customerDetails = []  #array to store the customer details
         print("Please enter the data as received in the survey.\n")        
         county = input("Enter county : ")
         vehicles = input("Enter number of vehicles : ")
@@ -77,13 +74,36 @@ def getUserDataInput():
         price = input("Enter in rating for price : ")
         sites = input("Enter in rating for sites : ")
         reliability = input("Enter in rating for reliability : ")
-        customerDetails.append(county, )
+        customerDetails.extend((county, vehicles, monthlyLiters, monthlyEuro, customerType, service, price, sites, reliability))
+        print(customerDetails)
+        if validateUserData(customerDetails):
+            print("Customer data is valid")
+            break
+
 
 def validateUserData(data):
     """
     Function to validate the data the user has entered to add in to the
     survey results.
+    Get the data in as a string, check to make sure it's a string, int, int,
+    int, string(either sole or ltd), then ints for all ratings
     """
+    detailsStrings = []
+    detailsInts = []
+    detailsStrings.extend((data[0], data[4]))
+    detailsInts.extend((data[1], data[2], data[3], data[5], data[6], data[7], data[8]))
+    # now there are 2 arrays - one for strings and one for integers
+    try:
+        for detail in detailsStrings:
+            if detail.isdigit():
+                raise ValueError("You have entered a number insteead of text")
+        for num in detailsInts:
+            if int(num):
+                raise ValueError("You have entered text indtead of a number")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.")
+        return False
+    return True
+
 getUserFunction()
 getUserDataInput()
-
